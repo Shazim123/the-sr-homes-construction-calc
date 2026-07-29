26a21b25521f3f506e113c7c058e34a4991e9e26
Shazim123 <128867420+Shazim123@users.noreply.github.com>
2026-07-29 12:51:00 +0530

    Add concrete volume calculation function

---
+++ concrete.js
@@ -0,0 +1,44 @@
+function calculateConcrete(){
+
+let L = Number(document.getElementById("length").value);
+let W = Number(document.getElementById("width").value);
+let D = Number(document.getElementById("depth").value);
+let Q = Number(document.getElementById("qty").value);
+
+let volume = L * W * D * Q;
+
+
+// Approximate nominal mix calculation
+// 1 m³ concrete ≈ 7.5 cement bags
+let cement = volume * 7.5;
+
+let sand = volume * 0.42;
+
+let aggregate = volume * 0.84;
+
+
+document.getElementById("result").innerHTML =
+
+`
+<h3>Result</h3>
+
+Concrete Volume:
+<b>${volume.toFixed(2)} m³</b>
+<br><br>
+
+Cement:
+<b>${cement.toFixed(1)} bags</b>
+
+<br><br>
+
+Sand:
+<b>${sand.toFixed(2)} m³</b>
+
+<br><br>
+
+Aggregate:
+<b>${aggregate.toFixed(2)} m³</b>
+
+`;
+
+}
