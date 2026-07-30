7d7ba0c4c28c901b383274e8175dd7c7d053a369
Shazim123 <128867420+Shazim123@users.noreply.github.com>
2026-07-30 10:32:17 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "sr-homes-v1";
+
+const filesToCache = [
+  "index.html",
+  "style.css",
+  "manifest.json"
+];
+
+
+self.addEventListener("install", function(event){
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(function(cache){
+
+return cache.addAll(filesToCache);
+
+})
+
+);
+
+});
+
+
+self.addEventListener("fetch", function(event){
+
+event.respondWith(
+
+caches.match(event.request)
+.then(function(response){
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
